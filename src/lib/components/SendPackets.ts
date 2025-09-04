a9b79af8ffad0b197cd02ca9347552b577cd567d
Marcelo Lago <72633651+mslago@users.noreply.github.com>
2025-09-04 02:49:25 +0100

    fix: fix base64 encoding of whitespaces and other non-ASCII characters of sent packets

--- src/lib/components/SendPackets.ts
+++ src/lib/components/SendPackets.ts
@@ -1,6 +1,15 @@
 import packets from "$lib/packets.json";
 import { json } from "@sveltejs/kit";
 
+function base64Encode(str: string) {
+  const encodedStr = encodeURIComponent(str);
+
+  const bytes = encodedStr.replace(/%([0-9A-F]{2})/g, (match, p1) =>
+    String.fromCharCode(parseInt(p1, 16))
+  );
+  return btoa(bytes);
+}
+
 export async function sendPacketsFromCode(
   code: string,
   server: string,
@@ -9,9 +18,7 @@ export async function sendPacketsFromCode(
   const found = packets.find((p) => p.code === code);
   if (!found) throw new Error("Packet code not found");
   const packetToSend = JSON.parse(JSON.stringify(found.packet));
-  packetToSend.payload.value = btoa(
-    encodeURIComponent(packetToSend.payload.value) // Encode the value to base64
-  );
+  packetToSend.payload.value = base64Encode(packetToSend.payload.value);
   packetToSend.timestamp = timestamp.toString();
   const response = await fetch(server, {
     method: "POST",
